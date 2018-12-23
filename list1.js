8934a388697717d6812116a9892fc56eb9ac5649
Tam <46091013+TaMmYzEn@users.noreply.github.com>
2018-12-24 00:42:07 +0530

    adding starting project

---
+++ list1.js
@@ -0,0 +1,25 @@
+function singIn(){
+
+let Name = "tam";
+let Password = "01012000";
+
+var prompt1=prompt("your name");
+var prompt2=prompt("your password");
+if (prompt1===Name && prompt2===Password) {
+
+		alert("you got a victory tam")
+	
+}
+else {
+  alert("wrong name")
+}
+}
+
+singIn();
+
+
+
+
+
+
+
